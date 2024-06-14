53029408ed046895e19b38b83e43662b0207aa1e
Daniel-Marciniak-developer <danielmarciniak.business@gmail.com>
2024-06-14 12:11:57 +0200

    Update globals.py

--- ProgramToHologram/globals.py
+++ ProgramToHologram/globals.py
@@ -4,9 +4,9 @@ class Globals:
 
     mesure_time_for_fist = 0
     mesure_time_for_calling = 0
-    openai_api_key = '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'
-    weather_api_key = '963e60c0883c01b9df64de639e1d3e89'
-    latitude, longitude = 51.749599, 18.049058
+    openai_api_key = #
+    weather_api_key = #
+    latitude, longitude = #
     base_url = "https://api.openweathermap.org/data/2.5/weather"
     rewrite_message_Ai = "Napisz to samo w maksymalnie dwóch zdaniach, liczby i czas napisz słownie z poprawną odmianą:"
     structure_massage_Ai = " Odpowiedz w maksymalnie dwóch zdaniach (Jesteś pomocnym asystentem), liczby napisz słownie z poprawną odmianą."
